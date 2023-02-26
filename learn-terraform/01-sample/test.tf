48c4e615da098eb5da7cb5dcb590ddf40e9ed10d
mukunda <suryaysr69@gmail.com>
2023-02-26 18:45:55 +0530

    terraform

--- learn-terraform/01-sample/test.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance""ec2"{
-  ami  ="Centos-8-DevOps-Practice"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0730089cc7ff79932]
-  tags ={
-    name="test"
-  }
-}
